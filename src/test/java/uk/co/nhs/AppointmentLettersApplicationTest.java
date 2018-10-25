5bb6550b3a4066acb7ec971aae6f042dcd47002e
suresh.averineni <suresh.averineni@gmail.com>
2018-10-25 14:58:36 +0100

    Code refactoring

--- src/test/java/uk/co/nhs/AppointmentLettersApplicationTest.java
+++ src/test/java/uk/co/nhs/AppointmentLettersApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringJpaHibernateExampleApplicationTests {
+public class AppointmentLettersApplicationTest {
 
 	@Test
 	public void contextLoads() {
