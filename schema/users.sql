2474dedab44c13079c21fe5e89b06a93529791d1
suresh.averineni <suresh.averineni@gmail.com>
2018-10-30 21:27:05 +0000

    mysql creation sql

--- schema/users.sql
+++ schema/users.sql
@@ -1,15 +1,20 @@
 DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `user` (
-  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `username` VARCHAR(255) UNIQUE,
-  `email` VARCHAR(255) UNIQUE,
-  `password` CHAR(60),
-  `firstName` VARCHAR(50),
-  `lastName` VARCHAR(50),
-  `enabled` BOOLEAN,
-  `createdOn` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `resetToken` CHAR(36)
+CREATE TABLE appointment_letters.`user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `createdOn` datetime NOT NULL,
+  `dateOfBirth` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `enabled` bit(1) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `resetToken` varchar(255) DEFAULT NULL,
+  `updatedOn` datetime DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
 );
 
 INSERT INTO `user` VALUES
